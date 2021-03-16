8b265ad00854f8e8b61428dea50bcc80f7e9568a
Chris Huckins <huckinsc@amazon.com>
2021-03-16 09:50:43 -0400

    Added back buttons to the controller sections

--- front-end/gameScript.js
+++ front-end/gameScript.js
@@ -4,7 +4,6 @@ document.getElementById("sell").addEventListener("click", switchToSellControl);
 document
   .getElementById("travel")
   .addEventListener("click", switchToTravelControl);
-
 //show buy control panel
 function switchToBuyControl() {
   document.getElementById("defaultControl").style.display = "none";
@@ -21,4 +20,24 @@ function switchToSellControl() {
 function switchToTravelControl() {
   document.getElementById("defaultControl").style.display = "none";
   document.getElementById("travelControl").style.display = "block";
+}
+
+// Setup back buttons
+document
+  .getElementById("buyBackBtn")
+  .addEventListener("click", handleBackBtnClick);
+
+document
+  .getElementById("sellBackBtn")
+  .addEventListener("click", handleBackBtnClick);
+
+document
+  .getElementById("travelBackBtn")
+  .addEventListener("click", handleBackBtnClick);
+
+function handleBackBtnClick() {
+  document.getElementById("defaultControl").style.display = "block";
+  document.getElementById("buyControl").style.display = "none";
+  document.getElementById("sellControl").style.display = "none";
+  document.getElementById("travelControl").style.display = "none";
 }
