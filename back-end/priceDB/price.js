e6e7111230979031f5cba40ad5f12d126c3ffca9
chenjr16 <chenjr16@gmail.com>
2021-03-16 12:34:20 -0700

    Added data to priceDB

--- back-end/priceDB/price.js
+++ back-end/priceDB/price.js
@@ -1,1 +1,71 @@
-const prices = {};
+const prices = [
+  {
+    city: "Honolulu",
+    prices: {
+      pineapple: 2,
+      apples: 5,
+      cherries: 8,
+      strawberries: 10,
+      keyLimes: 6,
+      avacadoes: 10,
+    },
+  },
+  {
+    city: "Seattle",
+    prices: {
+      pineapple: 5,
+      apples: 2,
+      cherries: 3,
+      strawberries: 8,
+      keyLimes: 8,
+      avacadoes: 15,
+    },
+  },
+  {
+    city: "New York",
+    prices: {
+      pineapple: 8,
+      apples: 3,
+      cherries: 5,
+      strawberries: 10,
+      keyLimes: 6,
+      avacadoes: 18,
+    },
+  },
+  {
+    city: "Los Angeles",
+    prices: {
+      pineapple: 4,
+      apples: 4,
+      cherries: 3,
+      strawberries: 3,
+      keyLimes: 3,
+      avacadoes: 3,
+    },
+  },
+  {
+    city: "Miami",
+    prices: {
+      pineapple: 3,
+      apples: 6,
+      cherries: 8,
+      strawberries: 5,
+      keyLimes: 3,
+      avacadoes: 4,
+    },
+  },
+  {
+    city: "Denver",
+    prices: {
+      pineapple: 10,
+      apples: 5,
+      cherries: 5,
+      strawberries: 4,
+      keyLimes: 6,
+      avacadoes: 12,
+    },
+  },
+];
+
+//export
+module.exports = prices;
